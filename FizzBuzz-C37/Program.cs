5e269e46a551d6662b2c93e61be266d87409a19e
Greg Doud <gdoud@gmail.com>
2022-05-04 09:24:37 -0400

    Add project files.

---
+++ FizzBuzz-C37/Program.cs
@@ -0,0 +1,15 @@
+﻿for (var idx = 1; idx <= 30; idx++) {
+    if (idx % 3 == 0 && idx % 5 == 0) {
+        Console.Write($"FIZZBUZZ, ");
+        continue;
+    }
+    if (idx % 3 == 0) {
+        Console.Write($"FIZZ, ");
+        continue;
+    }
+    if (idx % 5 == 0) {
+        Console.Write($"BUZZ, ");
+        continue;
+    }
+    Console.Write($"{idx}, ");
+}
